de3998efc1c588c365cfd8253017d7a49d10956f
aristianes <71615848+aristianes@users.noreply.github.com>
2022-08-16 16:47:34 -0500

    feature delete
    Created and tested.

--- src/main/java/com/aristiane/liquor/controller/CustomerController.java
+++ src/main/java/com/aristiane/liquor/controller/CustomerController.java
@@ -3,7 +3,9 @@ package com.aristiane.liquor.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +37,13 @@ public class CustomerController {
 		return this.service.findAll();
 		
 	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id){
+		
+		this.service.delete(id);
+	}
+	
 	
 
 }
--- src/main/java/com/aristiane/liquor/service/CustomerService.java
+++ src/main/java/com/aristiane/liquor/service/CustomerService.java
@@ -3,8 +3,6 @@ package com.aristiane.liquor.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import org.springframework.stereotype.Service;
 
 import com.aristiane.liquor.model.Customer;
@@ -25,8 +23,13 @@ public class CustomerService {
 	}
 
 	public List<Customer> findAll() {
-		
+
 		return (List<Customer>) this.customerRepository.findAll();
+	}
+
+	public void delete(long id) {
+		this.customerRepository.deleteById(id);
+
 	}
 
 }
