53dc0beb9786eac37010f63331f43d650e9cc423
aristianes <71615848+aristianes@users.noreply.github.com>
2022-08-17 15:40:54 -0500

    feature verbs employee
    created and tested.

--- src/main/java/com/aristiane/liquor/controller/EmployeeController.java
+++ src/main/java/com/aristiane/liquor/controller/EmployeeController.java
@@ -1,6 +1,11 @@
 package com.aristiane.liquor.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.aristiane.liquor.model.Employee;
 import com.aristiane.liquor.service.EmployeeService;
 
-
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService service;
-	
+
 	@PostMapping
 	public Employee save(@RequestBody Employee employee) {
 
@@ -25,6 +29,18 @@ public class EmployeeController {
 		return employee;
 
 	}
-	
 
+	@GetMapping
+	public List<Employee> findAll() {
+
+		return this.service.findAll();
+
+	}
+	
+	@DeleteMapping("/{id}")
+ public void delete(@PathVariable long id){
+		
+		this.service.delete(id);
+	}
+	
 }
--- src/main/java/com/aristiane/liquor/service/EmployeeService.java
+++ src/main/java/com/aristiane/liquor/service/EmployeeService.java
@@ -1,18 +1,19 @@
 package com.aristiane.liquor.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aristiane.liquor.model.Employee;
 import com.aristiane.liquor.repository.EmployeeRepository;
 
-
 @Service
 public class EmployeeService {
 
 	@Autowired
 	private EmployeeRepository EmployeeRepository;
-	
+
 	public Employee save(Employee employee) {
 
 		this.EmployeeRepository.save(employee);
@@ -21,5 +22,13 @@ public class EmployeeService {
 
 	}
 
-	
+	public List<Employee> findAll() {
+
+		return (List<Employee>) this.EmployeeRepository.findAll();
+	}
+
+	public void delete(long id) {
+		this.EmployeeRepository.deleteById(id);
+
+	}
 }
