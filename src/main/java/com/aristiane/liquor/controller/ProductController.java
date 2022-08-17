a1a6a9348273175b0361f51f19fe4acc4f37497a
aristianes <71615848+aristianes@users.noreply.github.com>
2022-08-17 17:13:29 -0500

    feature delete product
    tested and validated.

--- src/main/java/com/aristiane/liquor/controller/ProductController.java
+++ src/main/java/com/aristiane/liquor/controller/ProductController.java
@@ -3,7 +3,9 @@ package com.aristiane.liquor.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,11 @@ public class ProductController {
 
 		return this.service.findAll();
 
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+
+		this.service.delete(id);
 	}
 }
--- src/main/java/com/aristiane/liquor/service/ProductService.java
+++ src/main/java/com/aristiane/liquor/service/ProductService.java
@@ -27,4 +27,9 @@ public class ProductService {
 		return (List<Product>) this.ProductRepository.findAll();
 	}
 
+	public void delete(long id) {
+		this.ProductRepository.deleteById(id);
+
+	}
+
 }
